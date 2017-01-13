3eb3f1777d9954b4e393bbbbae28d93b8f48dd81
Pulkit Aggarwal <pulkit.cloud@gmail.com>
2017-01-14 04:41:11 +0530

    More APIs Added

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ angular.module('myApp', [])
       fetch();
     });
 
-    $scope.search = "Sherlock Holmes";
+    $scope.search = "Web Developer";
 
     function fetch(){
       // $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
       // .then(function(response){ $scope.details = response.data; });
 
-      $http.get("http://api.indeed.com/ads/apisearch?publisher=1637952116847595&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json")
+      // $http.get("http://api.indeed.com/ads/apisearch?publisher=1637952116847595&q=java&l=delhi&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&v=2&format=json")
+      $http.get("http://api.careerbuilder.com/v2/jobsearch?DeveloperKey=WDHN5J75ZPHLGQVZHDN8&outputjson=true")
       .then(function(response){ $scope.jobs = response.data; });
     }
 
